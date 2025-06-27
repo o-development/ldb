4135e6b8eef969fe8c588b051101a95c919e8b2d
Jackson Morgan <jaxoncreed@gmail.com>
2025-06-26 21:54:08 -0400

    Component library builds

---
+++ packages/component/tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js (only used during development/building)
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"], // your source files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
